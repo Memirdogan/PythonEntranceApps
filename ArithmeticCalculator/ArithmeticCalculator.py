876735868d1f85d2fab795ce93c032baf94c04fa
Emir <musaemird@gmail.com>
2024-04-30 18:36:12 +0300

    functions

---
+++ ArithmeticCalculator/ArithmeticCalculator.py
@@ -0,0 +1,17 @@
+# FUNCS
+def topla(x,y):
+    return x + y
+
+def cıkart(x,y):
+    return x - y
+
+def carp(x,y):
+    return x * y
+
+def bol(x,y):
+    if y == 0:
+        return "sıfıra bolme hatası"
+    else:
+        return x / y
+
+# input
