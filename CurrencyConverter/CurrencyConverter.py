3d3b0f5454a84be10ccdc51f943c588cbf71d9eb
Emir <musaemird@gmail.com>
2024-05-02 19:11:13 +0300

    update class and form type

--- CurrencyConverter/CurrencyConverter.py
+++ CurrencyConverter/CurrencyConverter.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 import requests
 
-class RealTimeCurrencyConverter():
-    def __init__(self,url):
+
+class RealTimeCurrencyConverter:
+    def __init__(self, url):
         self.data = requests.get(url).json()
         self.tokens = self.data["rates"]
         self.baseCurrency = "TL"
@@ -13,3 +14,28 @@ class RealTimeCurrencyConverter():
             amount = amount / self.tokens[fromCurrency]
         amount = round(amount * self.tokens[toCurrency], 4)
         return amount
+
+
+url = "https://api.exchangerate-api.com/v4/latest/TRY"
+converter = RealTimeCurrencyConverter(url)
+
+
+# Create UI
+
+class converterUI(tk.Tk):
+    def __init__(self, converter):
+        tk.Tk.__init__(self)
+        self.title = "Döviz çevirici"
+        self.currentlyConverter = converter
+
+        self.geometry("500x200")
+
+        self.introLabel = tk.Label(self, text="Döviz kuru hesaplayıcıya hoşgeldiniz", fg="yellow", relief=tk.RAISED,
+                                   borderwidth=3)
+        self.introLabel.config(font="Times 15 bold")
+
+        self.dateLabel = tk.Label(self,
+                                  text=f"1 Türk lirası = {self.currentlyConverter.convert('USD', 'TRY', 1)} USD \n Date : {self.currentlyConverter.data['date']}",
+                                  relief=tk.GROOVE, borderwidth=5)
+        self.introLabel.place(x = 10, y = 5)
+        self.dateLabel.place(x = 170, y =50)
