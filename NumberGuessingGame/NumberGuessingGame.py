f66a99849158c0fe43d615f7773808649d2524d5
Emir <musaemird@gmail.com>
2024-05-09 21:20:10 +0300

    oyun çalışır hale getirildi

--- NumberGuessingGame/NumberGuessingGame.py
+++ NumberGuessingGame/NumberGuessingGame.py
@@ -5,29 +5,19 @@ from tkinter import ttk
 
 class NumberGuess:
     def __init__(self):
+        self.correct_button = None
+        self.upper_button = None
+        self.lower_button = None
+        self.textlabel = None
+        self.label_guess = None
+        self.guess = None
         self.lower_bound = 1
         self.upper_bound = 100
 
-    """
-        while True:
-            guess = random.randint(self.lower_bound, self.upper_bound)
-            print(f"Sayımız: {guess} mi")
-
-            forecast = input("Daha küçük ise (K),\nDaha büyük ise (B)\nDoğru bilgiysem (D) basınız")
-
-            if forecast == "d" or forecast == "D":
-                print("Tahmin etmeyi başardım, yuppii ^^")
-                break
-            elif forecast == "b" or forecast == "B":
-                lower_bound = guess + 1
-            elif forecast == "k" or forecast == "K":
-                upper_bound = guess - 1
-            else:
-                print("Hatalı tuşlama yaptınız!")
-    """
     def start_game(self, form):
+
         # guess label
-        self.label_guess = ttk.Label(form, text="aaaaaaaaaaaaaaaaaaaaaaa", background="whitesmoke")
+        self.label_guess = ttk.Label(form, text="", background="whitesmoke")
         self.label_guess.configure(style="TLabel", font=("Courier", 14, 'bold'), justify=tk.CENTER)
         self.label_guess.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
@@ -37,17 +27,39 @@ class NumberGuess:
         self.textlabel.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
 
         # lower button
-        self.lower_button = ttk.Button(form, text="Aşağı", padding=(14, 7))#, command=self.lower_guess)
+        self.lower_button = ttk.Button(form, text="Aşağı", padding=(14, 7), command=self.lower_guess)
         self.lower_button.place(relx=0.25, rely=0.6, anchor=tk.CENTER)
 
         # upper button
-        self.upper_button = ttk.Button(form, text="Yukarı", padding=(14, 7))#, command=self.lower_guess)
+        self.upper_button = ttk.Button(form, text="Yukarı", padding=(14, 7), command=self.upper_guess)
         self.upper_button.place(relx=0.75, rely=0.6, anchor=tk.CENTER)
 
+        # correct button
+        self.correct_button = ttk.Button(form, text="Bildin!", padding=(12, 6), command=self.correct_guess)
+        self.correct_button.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
+        self.guess_number()
+
+    def lower_guess(self):
+        self.upper_bound = self.guess - 1
+        self.guess_number()
+
+    def upper_guess(self):
+        self.lower_bound = self.guess + 1
+        self.guess_number()
+
+    def correct_guess(self):
+        self.textlabel.config(text="Tahmin etmeyi başardım, yuppii ^^")
+
+    def guess_number(self):
+        self.guess = random.randint(self.lower_bound, self.upper_bound)
+        self.label_guess.config(text=f"{self.guess}")
 
 class NumberGuessUI:
     def __init__(self, form):
+        self.start_button = None
+        self.desclabel = None
+        self.introlabel = None
         self.form = form
         self.form.title("Sayı Tahmin Oyunu")
         self.number_guess = NumberGuess()
@@ -85,7 +97,6 @@ class NumberGuessUI:
         self.start_button.destroy()
         self.desclabel.destroy()
         self.number_guess.start_game(self.form)
-
 
 if __name__ == '__main__':
     form = tk.Tk()
