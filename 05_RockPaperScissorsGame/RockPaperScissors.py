2edd0b89f9f32d81529c780bf319e39e17daf644
Emir <musaemird@gmail.com>
2024-05-13 19:58:11 +0300

    final ver

--- 05_RockPaperScissorsGame/RockPaperScissors.py
+++ 05_RockPaperScissorsGame/RockPaperScissors.py
@@ -12,44 +12,76 @@ class rpsgame():
         self.playgame()
 
     def playgame(self):
-        score_bot = 0
-        score_user = 0
-        print("_____________")
-        print("Bot'un Skoru: ", score_bot)
-        print("Senin Skorun: ", score_user)
-        print("_____________")
-
-        while score_user or score_bot < 3:
+        while True:
             index = 0
-            index += 1
+            score_bot = 0
+            score_user = 0
+            print("_____________")
+            print("Bot'un Skoru: ", score_bot)
+            print("Senin Skorun: ", score_user)
+            print("_____________")
 
-            print(index,".Round!")
-            botmove = self.movesnum()
-            usermove = input("Taş mı, Kağıt mı, Makas mı?").lower()
+            while score_user < 3 and score_bot < 3:
 
+                index += 1
 
-            print("Bot'un Seçimi: ", botmove)
+                print(index, ".Round!")
+                botmove = self.movesnum()
+                time.sleep(0.5)
+                usermove = input("Taş mı, Kağıt mı, Makas mı?").lower()
 
-            if usermove == botmove:
-                print("---BERABERE PUAN YOK---")
-            elif usermove == "taş":
-                if botmove == "kağıt":
-                    print("Bot, taşını kağıt ile sarıp yere fırlattı :(")
-                    score_bot += 1
-                elif botmove == "makas":
-                    print("")
+                print("\n")
+                print("Bot'un Seçimi: ", botmove)
+                time.sleep(1)
 
-
+                if usermove == botmove:
+                    print("---BERABERE PUAN YOK---")
+                    print("\n")
+                elif usermove == "taş":
+                    if botmove == "kağıt":
+                        print("Bot, taşını kağıt ile sarıp yere fırlattı :(")
+                        score_bot += 1
+                    elif botmove == "makas":
+                        print("Taşı eline alıp bot'un makasını kırdın, süpersin!")
+                        score_user += 1
                 elif usermove == "kağıt":
-                    print("Kağıt ile taşı sardın, iyi gidiyorsun!")
-                    score_user += 1
+                        if botmove == "makas":
+                            print("Bot makasını kullanarak senin kağıdını kesti :(")
+                            score_bot += 1
+                        elif botmove == "taş":
+                            print("Kağıt ile taşı sardın, iyi gidiyorsun!")
+                            score_user += 1
                 elif usermove == "makas":
-                    print("Bot taş ile makasını kırdı :(")
-                    score_bot += 1
+                    if botmove == "taş":
+                        print("Bot, taş ile makasını kırdı :(")
+                        score_bot += 1
+                    elif botmove == "kağıt":
+                        print("Makası eline alıp bot'un kağıdını kestin, harikasın!")
+                        score_user += 1
                 else:
                     print("taş, kağıt ya da makası seçmen lazım? DÜZGÜN YAZ!")
 
+                time.sleep(1.2)
+                print("_____________")
+                print("Bot'un Skoru: ", score_bot)
+                print("Senin Skorun: ", score_user)
+                print("_____________")
 
+            if score_user > score_bot:
+                print("BOT'U YENDİN, TEBRİK EDERİM !!!!!!")
+                time.sleep(3)
+            else:
+                print("ÜZGÜNÜM AMA DÜNYANIN EN ZEKİ PYTHON BOTUNA KAYBETTİN :(")
+                time.sleep(3)
+
+
+            resume = input("Oyuna devam etmek istiyor musunuz? (Evet/Hayır): ").lower()
+            if resume != "evet":
+                print("Oyunu bitirdiniz. İyi günler!")
+                break
+            else:
+                print("Oyuna devam ediliyor...")
+                time.sleep(1)
 
     def movesnum(self):
         moves = [
@@ -58,19 +90,6 @@ class rpsgame():
             "makas"
         ]
         return random.choice(moves)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
