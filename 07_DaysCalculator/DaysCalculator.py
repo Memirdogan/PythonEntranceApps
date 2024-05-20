765e8fec74b03899afe2f47f0f7f48825f68270f
Emir <musaemird@gmail.com>
2024-05-20 20:33:53 +0300

    calculate fonksiyonu oluşturulmaya çalışıldı kütüphane import edildi

--- 07_DaysCalculator/DaysCalculator.py
+++ 07_DaysCalculator/DaysCalculator.py
@@ -1,14 +1,17 @@
 from datetime import date
 import tkinter as tk
 from tkinter import ttk
+from tkcalendar import DateEntry
 
 
 class TimeCalculate:
     def __init__(self):
+        self.calender_label = None
         self.frame = None
         self.form = tk.Tk()
         self.form.title("Gün Hesaplayıcı")
-        self.create_widgets()
+        self.calculate()
+        self.form.mainloop()
 
     def create_widgets(self):
         self.form.style = ttk.Style()
@@ -18,9 +21,12 @@ class TimeCalculate:
         self.frame.pack(fill=tk.BOTH, expand=True)
 
         self.form.geometry("500x200")
-        self.form.mainloop()
+
+    def calculate(self):
+        self.calender_label = ttk.Label(self.frame, text="tarih seç: ")
+        self.calender_label.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
 
 if __name__ == '__main__':
     calculate = TimeCalculate()
-    calculate.start()
+    calculate.create_widgets()
