930a324e177cdbb5dd866ffa3ac9088596fc21ef
Emir <musaemird@gmail.com>
2024-06-09 21:08:25 +0300

    add insertion_sort

--- 08_SortingAlgorithms/SortingAlgorithms.py
+++ 08_SortingAlgorithms/SortingAlgorithms.py
@@ -1,19 +1,30 @@
-
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        for j in range(0,n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
 
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
         idx_min = i
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             if arr[j] < arr[idx_min]:
                 idx_min = j
         arr[i], arr[idx_min] = arr[idx_min], arr[i]
+    return arr
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
     return arr
 
