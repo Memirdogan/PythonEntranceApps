c992a48d720e5a17ed70f534f97f917f6be63efe
Emir <musaemird@gmail.com>
2024-06-09 20:55:01 +0300

    add selection_sort

--- 08_SortingAlgorithms/SortingAlgorithms.py
+++ 08_SortingAlgorithms/SortingAlgorithms.py
@@ -7,3 +7,13 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        idx_min = i
+        for j in range(i+1, n):
+            if arr[j] < arr[idx_min]:
+                idx_min = j
+        arr[i], arr[idx_min] = arr[idx_min], arr[i]
+    return arr
+
