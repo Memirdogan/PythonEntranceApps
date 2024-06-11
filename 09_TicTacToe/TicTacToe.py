5c181a7ee5d3f84f6d8eae6d2f296bf73a4e4a3a
Emir <musaemird@gmail.com>
2024-06-11 17:30:28 +0300

    create board

---
+++ 09_TicTacToe/TicTacToe.py
@@ -0,0 +1,12 @@
+
+board = [" " for _ in range(9)]
+
+def print_board(board):
+    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print("--+---+--")
+    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print("--+---+--")
+    print(f"{board[6]} | {board[7]} | {board[8]}")
+
+print_board(board)
+
