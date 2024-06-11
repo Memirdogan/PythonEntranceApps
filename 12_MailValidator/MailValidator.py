4a07b5ff85b7f008ac9bf804e214f752e3ccdd28
Emir <musaemird@gmail.com>
2024-06-11 21:29:57 +0300

    final push

---
+++ 12_MailValidator/MailValidator.py
@@ -0,0 +1,26 @@
+
+def addressVal(mail):
+    dot = mail.find(".")
+    at = mail.find("@")
+
+    if dot == -1:
+        print("Geçersiz mail adresi")
+    elif at == -1:
+        print("Geçersiz mail adresi")
+    else:
+        print("Geçerli mail adresi")
+
+def main():
+    print("Bu program girdiğiniz bilginin geçerli bir e-posta adresi olup olmadığına karar verecektir")
+    print()
+    while True:
+        print("**********************************************************************")
+        print("Geçerli bir e-posta adresinde '@' sembolü ve '.' işareti bulunmalıdır.")
+        print("**********************************************************************")
+        print()
+        mail = input("Mail adresinizi giriniz: ")
+        addressVal(mail)
+
+
+if __name__ == '__main__':
+    main()
